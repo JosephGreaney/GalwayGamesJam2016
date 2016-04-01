c59683e9440b77dd300699d5b7441fa480dae306
JosephGreaney <josephgreaney@gmail.com>
2016-04-01 21:33:50 +0100

    Added Scripts, Character Controls and materials
    Added game management scripts Loader and GameManager.
    Added physics materials.
    Added scripts to control the player character.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**
+ *  The GameManager class manages GameObjects
+ */
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;  // static instance of GameManager which allows it to be a singleton
+
+    /**
+     *  Awake is called before Start functions
+     */
+    void Awake ()
+    {
+        //Check if instance already exists
+        if (instance == null)
+            instance = this;        //If not set instance to this
+        
+        else if (instance != this)
+            Destroy(gameObject);    //Otherwise destroy this
+
+        //Sets this object to not be destroyed when scene is reloaded
+        DontDestroyOnLoad(gameObject);
+	}
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ *  The loader class is used to instantiate GameObjects
+ */
+public class Loader : MonoBehaviour
+{
+
+    public GameObject gameManager;      //GameManager prefab
+
+    /**
+     *  Awake is used to instantiate game objects
+     */
+    void Awake()
+    {
+        // Check if GameManager has already been instantiated
+        if (GameManager.instance == null)
+            Instantiate(gameManager);
+    }
+}
