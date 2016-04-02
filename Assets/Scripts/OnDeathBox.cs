4cce8f34bdf7c974af0431d72e0c0b20c7ef62f7
JosephGreaney <josephgreaney@gmail.com>
2016-04-02 12:51:52 +0100

    Added Death Box collision
    The OnDeathBox script can be attached to a collider which kills the
    player when they collide with it.

---
+++ Assets/Scripts/OnDeathBox.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * This class is used to handle the collision detection for boxes that kill the player
+ * when the player collides with this script's trigger box then the scene is restarted
+*/
+public class OnDeathBox : MonoBehaviour {
+
+    // If the player enters with the trigger box then the player is killed
+	void OnTriggerEnter2D(Collider2D coll) {
+        
+		if (coll.gameObject.tag == "Player") {
+            //Kill the player
+            GameManager.instance.PlayerDeath();
+        }
+	}
+}
