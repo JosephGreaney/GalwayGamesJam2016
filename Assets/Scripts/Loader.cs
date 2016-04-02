231c66e1ce914a528b8774df93052f350f507627
JosephGreaney <josephgreaney@gmail.com>
2016-04-02 12:04:14 +0100

    Added Sound Manager
    The sound manager script manages the audio sources of the game.

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -8,6 +8,7 @@ public class Loader : MonoBehaviour
 {
 
     public GameObject gameManager;      //GameManager prefab
+    public GameObject soundManager;     //SoundManager prefab
 
     /**
      *  Awake is used to instantiate game objects
@@ -17,5 +18,7 @@ public class Loader : MonoBehaviour
         // Check if GameManager has already been instantiated
         if (GameManager.instance == null)
             Instantiate(gameManager);
+        if (SoundManager.instance == null)
+            Instantiate(soundManager);
     }
 }
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**
+ *  The SoundManager class manages the sounds of the game
+ */
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance = null;  // static instance of SoundManager which allows it to be a singleton
+    public AudioSource musicSource;
+    public AudioSource effectSource;
+    
+    /**
+     *  Awake is called before Start functions
+     */
+    void Awake ()
+    {
+        //Check if instance already exists
+        if (instance == null)
+            instance = this;        //If not set instance to this
+        
+        else if (instance != this)
+            Destroy(gameObject);    //Otherwise destroy this
+
+        //Sets this object to not be destroyed when scene is reloaded
+        DontDestroyOnLoad(gameObject);
+	}
+
+    /**
+     *  
+     */
+    public void PlayEffect(AudioClip clip)
+    {
+        effectSource.clip = clip;
+
+        //Play the clip.
+        effectSource.Play();
+    }
+}
