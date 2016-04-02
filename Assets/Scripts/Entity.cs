22290cdc40a4a04e53fd3d5a60c12005c3f44610
JosephGreaney <josephgreaney@gmail.com>
2016-04-02 12:31:52 +0100

    Fixed an issue with warping
    Fixed an issue where the player was unable to warp.

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -4,38 +4,36 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace UnityStandardAssets._2D
+public class Entity : MonoBehaviour
 {
-    public class Entity : MonoBehaviour
+
+    private static int ID;
+
+    private int id = ID++;
+    public int health;
+    public int damage;
+
+
+
+    private EntityType type;
+    private enum EntityType
+    {
+        PLAYER, WIZARD, DINOSAUR, CAVEMAN, ROBOT, ALIEN
+    };
+    // Use this for initialization
+    void Start()
     {
 
-        private static int ID;
+    }
 
-        private int id = ID++;
-        public int health;
-        public int damage;
+    // Update is called once per frame
+    void Update()
+    {
 
+    }
 
-        private EntityType type;
-        private enum EntityType
-        {
-            PLAYER, WIZARD, DINOSAUR, CAVEMAN, ROBOT, ALIEN
-        };
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        protected void Attack()
-        {
-            print(type + " attacked\n");
-        }
+    protected void Attack()
+    {
+        print(type + " attacked\n");
     }
 }
--- Assets/Scripts/PlatformerCharacter2D.cs
+++ Assets/Scripts/PlatformerCharacter2D.cs
@@ -4,112 +4,146 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace UnityStandardAssets._2D
+
+public class PlatformerCharacter2D : Entity
 {
-    public class PlatformerCharacter2D : Entity
+    [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+    [SerializeField] private float m_JumpForce = 600f;                  // Amount of force added when the player jumps.
+    [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
+    [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+
+    public float zoneDistance = 500;    // The distance between each zone in the level
+
+    private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
+    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool m_Grounded;            // Whether or not the player is grounded.
+    private bool m_Attacking;
+
+    private Rigidbody2D m_Rigidbody2D;
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+
+    private int currZone = 1;           // The current zone that the player is in
+
+    private void Awake()
     {
-        [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
-        [SerializeField] private float m_JumpForce = 600f;                  // Amount of force added when the player jumps.
-        [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
-        [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+        // Setting up references.
+        m_GroundCheck = transform.Find("GroundCheck");
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
 
-        private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
-        const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-        private bool m_Grounded;            // Whether or not the player is grounded.
-        private bool m_Attacking;
 
-        private Rigidbody2D m_Rigidbody2D;
-        private bool m_FacingRight = true;  // For determining which way the player is currently facing.
-
-        private int keys = 0;
-
-        private void Awake()
+    private void FixedUpdate()
+    {
+        m_Grounded = false;
+        // This is used to check if the feet of the player are touching ground.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
         {
-            // Setting up references.
-            m_GroundCheck = transform.Find("GroundCheck");
-            m_Rigidbody2D = GetComponent<Rigidbody2D>();
+            if (colliders[i].gameObject != gameObject)
+                m_Grounded = true;
         }
 
+        transform.rotation = Quaternion.Euler(0, 0, 0);
 
-        private void FixedUpdate()
+    }
+
+    public void Move(float move, bool jump, int warpDest)
+    {
+        // Check if the vertical speed will be below the limit
+        // Only control the player if grounded or airControl is turned on
+        if (m_Grounded || m_AirControl)
         {
-            m_Grounded = false;
-            // This is used to check if the feet of the player are touching ground.
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-            for (int i = 0; i < colliders.Length; i++)
-            {
-                if (colliders[i].gameObject != gameObject)
-                    m_Grounded = true;
-            }
-
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-
+            // Move the character
+            m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
         }
 
-		public void Move(float move, bool jump)
-		{
-			// Check if the vertical speed will be below the limit
-            // Only control the player if grounded or airControl is turned on
-            if (m_Grounded || m_AirControl)
-            {
-                // Move the character
-                m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
-            }
-
-            if (m_Grounded &&jump)
-            {
-                makeJump();
-            }
-
-            //If the player should jump ...
-            if (move > 0 && !m_FacingRight)
-            {
-                // ... flip the player.
-                Flip();
-                
-            }
-            // Otherwise if the input is moving the player left and the player is facing right...
-            else if (move < 0 && m_FacingRight)
-            {
-                // ... flip the player.
-                Flip();
-            }
-        }
-
-
-        private void Flip()
+        if (m_Grounded && jump)
         {
-            // Switch the way the player is labelled as facing.
-            m_FacingRight = !m_FacingRight;
-
-            // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
-            theScale.x *= -1;
-            transform.localScale = theScale;
+            makeJump();
         }
 
-
-        void OnTriggerEnter2D(Collider2D other)
+        //If the player should jump ...
+        if (move > 0 && !m_FacingRight)
         {
-            if (other.gameObject.CompareTag("PickUp"))
-            {
-                other.gameObject.SetActive(false);
-                keys++;
-            }
+            // ... flip the player.
+            Flip();
+
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (move < 0 && m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
         }
 
-        void makeJump()
-        {
-            // Add a vertical force to the player.
-            m_Grounded = false;
-            //m_Anim.SetBool("Ground", false);
-            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-        }
+        // If a warp destination has been set
+        if (warpDest != -1)
+            Warp(warpDest);
+    }
 
-        public void Attack(bool m_Attack)
+    /**
+     *  This function allows the player to warp between time zones
+     *  based on the y-axis
+     */
+    void Warp(int destination)
+    {
+        int warpTo = destination - currZone;
+        // Where the player will be warped to.
+        Vector3 targetDest = new Vector3(transform.position.x, (transform.position.y + (warpTo * zoneDistance)), transform.position.z);
+        
+        // Check if the destination is unoccupied
+        if (!CheckIfOccupied(targetDest))
         {
-            if(m_Attack && !m_Attacking)
-                base.Attack();
+            // If unoccupied move to that position
+            gameObject.transform.position = targetDest;
+            currZone = destination;
         }
+        else
+        {
+            Debug.Log("Collided");
+        }
+    }
+
+    /**
+     *  Check if a space in the world is occupied by any colliders
+     */
+    bool CheckIfOccupied(Vector3 targetDest)
+    {
+        bool occupied = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(targetDest, 0.5f);
+        // If you have collided with something
+        if (colliders.Length > 0)
+            occupied = true;
+        return occupied;
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+    }
+
+    void makeJump()
+    {
+        // Add a vertical force to the player.
+        m_Grounded = false;
+        //m_Anim.SetBool("Ground", false);
+        m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+    }
+
+    public void Attack(bool m_Attack)
+    {
+        if(m_Attack && !m_Attacking)
+            base.Attack();
     }
 }
