0d075a3ab21aecd52f1dd9f82ee3e9eef8a80dac
xskaterboy118x <chrisbrady118@gmail.com>
2016-04-03 10:03:20 +0100

    Slight update to last

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Elevator : MonoBehaviour
+{
+    public Transform farEnd;
+    private Vector3 frometh;
+    private Vector3 untoeth;
+    private float secondsForOneLength = 12f;
+
+    void Start()
+    {
+        frometh = transform.position;
+        untoeth = farEnd.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(frometh, untoeth,
+         Mathf.SmoothStep(0f, 1f,
+          Mathf.PingPong(Time.time / secondsForOneLength, 1f)
+        ));
+    }
+}
